b9e65b9b92b28d26654731d0129834c7e8dc17c6
Komachi Onozuka <onozuka.komachi@outlook.com>
2021-06-18 14:49:59 +0300

    Project Skeleton

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 app = Flask("World")
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return render_template("index.html", title="Hello")
